95a08bd871103c04e0bfbd1458c2747f19d947d5
egitofilho <egitofilho4@gmai.com>
2025-08-21 18:43:31 -0300

    exercicio 20/08

--- python/aula_3/exercicios.py
+++ python/aula_3/exercicios.py
@@ -117,8 +117,52 @@ print("Variável2", var2)
 # • o percentual de aumento aplicado;
 # • o valor do aumento;
 # • o novo salário, após o aumento.
-# TODO()
+
+salario = float(input("Digite o salário do colaborador: "))
+if salario <= 280:
+    aumento = salario * 1.20
+    print("Salário antes do reajuste: R$", salario)
+    print("Percentual de aumento aplicado: 20%")
+    print("Valor do aumento: R$", salario * 0.20)
+    print("Novo salário, após o aumento: R$", round(aumento, 2))
+elif salario > 280 and salario <= 700:
+    aumento = salario * 1.15
+    print("Salário antes do reajuste: R$", salario)
+    print("Percentual de aumento aplicado: 15%")
+    print("Valor do aumento: R$", salario * 0.15)
+    print("Novo salário, após o aumento: R$", round(aumento, 2))
+elif salario > 700 and salario <= 1500:
+    aumento = salario * 1.10
+    print("Salário antes do reajuste: R$", salario)
+    print("Percentual de aumento aplicado: 10%")
+    print("Valor do aumento: R$", salario * 0.10)
+    print("Novo salário, após o aumento: R$", round(aumento, 2))
+else:
+    aumento = salario * 1.05
+    print("Salário antes do reajuste: R$", salario)
+    print("Percentual de aumento aplicado: 5%")
+    print("Valor do aumento: R$", salario * 0.05)
+    print("Novo salário, após o aumento: R$", round(aumento, 2))
 
 #12 Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e
 # calcule a sua média. A atribuição de conceitos deve obedecer à tabela acima
-# TODO()
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+media = (nota1 + nota2) / 2
+
+if media >= 9.0 and media <= 10.0:
+    conceito = "A"
+    print(conceito)
+elif media >= 7.5 and media < 9.0:
+    conceito = "B"
+    print(conceito)
+elif media >= 6.0 and media < 7.5:
+    conceito = "C"
+    print(conceito)
+elif media >= 4.0 and media < 6.0:
+    conceito = "D"
+    print(conceito)
+elif media >= 0.0 and media < 4.0:
+    conceito = "E"
+    print(conceito)
