1b73fa9d8f1cbed20a0be4adf7ab30b9e05d9a9c
egitofilho <egitofilho4@gmai.com>
2025-09-20 17:12:35 -0300

    udemy paste

---
+++ java/curso_udemy/3 - Promoção automatica/promo.java
@@ -0,0 +1,11 @@
+class Promo {
+
+    byte a = 10;
+    int b = a + 5; 
+    double c = b + 5.5;
+
+    int d = 10;
+    float e = 5.5f;
+
+    float f = d + e;
+}
---
+++ java/curso_udemy/Exercicios/rectangule.java
@@ -0,0 +1,9 @@
+class Rectangule {
+    double width = 5.5;
+    double height = 8.0;
+    double area = width * height;
+
+    public void printArea() {
+        System.out.println("Área do retângulo: " + area);
+    }
+}
