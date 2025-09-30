766be5330b30b52f7a712aa953f4c6e96d59169f
J Egito GS Filho <egitofilho4@gmail.com>
2025-09-29 21:03:16 -0300

    primitive type

---
+++ java/rocket/meu-programa/src/primitive_tipe/TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package primitive_tipe;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        int number = 32;
+        double saldo = 123.00;
+        long saldo2 = 3213L;
+        float saldo3 = 32323f;
+        boolean estado = true;
+        char name = 'A';
+        String name2 = "Egito";
+    }
+}
